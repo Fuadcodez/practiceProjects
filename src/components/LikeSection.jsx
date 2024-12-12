fe26094e25d85d795c3ad9bf98b351b17baa2ed1
fuadcodez <fuadabolaji1@gmail.com>
2024-12-12 23:36:12 +0100

    made some changes to the post card

--- src/components/LikeSection.jsx
+++ src/components/LikeSection.jsx
@@ -19,16 +19,17 @@ const LikeSection = ({id}) => {
       if (error) console.error(error);
       else {setLikes(data)
         console.log('data added');
-    };}
-    console.log(id)
-    fetchLikes();
-  }, [id]);
-
-  // Check if the current user has liked the post
-  useEffect(() => {
-    if (likes.some((like) => like.user_id === session?.user?.id)) {
+      };}
+      fetchLikes();
+    }, [id]);
+    
+    // Check if the current user has liked the post
+    useEffect(() => {
+    console.log(likes)
+    if (likes.some((like) => like.username === session?.user?.username)) {
       setHasLiked(true);
       console.log(`he has liked ${id}`)
+      console.log(session.user.id)
     } else {
       setHasLiked(false);
       console.log(`he has not liked ${id}`)
@@ -41,13 +42,13 @@ const LikeSection = ({id}) => {
         .from('likes')
         .delete()
         .eq('post_id', id)
-        .eq('user_id', session.user.id);
+        .eq('username', session.user.username);
 
       if (error) {
         console.error(error);
       } else {
         setLikes((prevLikes) =>
-          prevLikes.filter((like) => like.user_id !== session.user.id)
+          prevLikes.filter((like) => like.username !== session.user.username)
         );
       }
     } else {
@@ -93,10 +94,13 @@ const LikeSection = ({id}) => {
   //   }
   // }
   return (
-    <div>
+    <div className='flex items-center gap-1'>
         {session&&<div className='flex border-t border-gray-100'>
             {hasLiked? (<HiHeart onClick={likePost} className='text-red-500 cursor-pointer text-3xl hover:scale-125 transition-transform duration-200 ease-out'/>): (<HiOutlineHeart onClick={likePost} className='cursor-pointer text-3xl hover:scale-125 transition-transform'/>)}
             </div>}
+            {likes.length > 0 && (<p className='text-gray-500'>
+              {likes.length} {likes.length > 1 ? 'likes': 'like'}
+            </p>)}
     </div>
   )
 }
